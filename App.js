f7139220a81e6060b5c5794850f337767f37b858
partho0 <yeasin.udoy89@gmail.com>
2022-12-07 21:16:40 +0600

    Push

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './component/Nav.js'
+import Main from './component/Main.js'
+
+export default function App(){
+    return(
+        <div className="container">
+        <Nav />
+        <Main />
+        </div>
+    )
+}
+
+ 
---
+++ component/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav(){
+    return(
+        <nav>
+        <img className="logo" src="./react.png"/>
+        <h3 className="logo-title">React Main Components</h3>
+        <h4 className="logo-nav">React Course Development</h4>
+        </nav>
+    )
+}
